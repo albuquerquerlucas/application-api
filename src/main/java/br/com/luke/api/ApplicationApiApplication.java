4d34552e07202358e1c1560cc00c6e487d3e76fe
Lucas Albuquerque <lucas.albuquerque@iviatecnologia.com.br>
2019-03-13 15:54:31 -0300

    remove Msg in class Application Start

--- src/main/java/br/com/luke/api/ApplicationApiApplication.java
+++ src/main/java/br/com/luke/api/ApplicationApiApplication.java
@@ -8,7 +8,6 @@ public class ApplicationApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApplicationApiApplication.class, args);
-		System.out.println("Init Project");
 	}
 
 }
