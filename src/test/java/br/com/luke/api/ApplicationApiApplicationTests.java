40b6b0ecc4e5ce974ee4900cf8eca569eaf04da5
Lucas Albuquerque <lucas.albuquerque@iviatecnologia.com.br>
2019-03-13 15:43:07 -0300

    Versão inicial do projeto

---
+++ src/test/java/br/com/luke/api/ApplicationApiApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.luke.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
