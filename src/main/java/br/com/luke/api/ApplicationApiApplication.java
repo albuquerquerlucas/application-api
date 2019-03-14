78332265e6cef4cfdb82ea5df6011059f904eed1
Lucas Albuquerque <lucas.albuquerque@iviatecnologia.com.br>
2019-03-14 10:32:19 -0300

    Trabalhando com perfis e cryptografia

--- src/main/java/br/com/luke/api/ApplicationApiApplication.java
+++ src/main/java/br/com/luke/api/ApplicationApiApplication.java
@@ -1,13 +1,44 @@
 package br.com.luke.api;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import br.com.luke.api.utils.SenhaUtils;
 
 @SpringBootApplication
 public class ApplicationApiApplication {
-
+		
+	@Value("${paginacao.qtd_por_pagina}")
+	private int qtdPorPagina;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ApplicationApiApplication.class, args);
 	}
-
+	
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args ->{
+			//System.out.println("### Quantidade de elementos por página = " + this.qtdPorPagina);
+			
+			testeEncodedSenha();
+		};
+	}
+	
+	/**
+	 * Teste de encryptação de senha com o BCrypt
+	 * do próprio Spring Security
+	 * 
+	 * */
+	public void testeEncodedSenha() {
+		String senhaEncoded = SenhaUtils.gerarBCrypt("123456");
+		System.out.println("Senha encoded: " + senhaEncoded);
+		
+		senhaEncoded = SenhaUtils.gerarBCrypt("123456");
+		System.out.println("Senha encoded novamente: " + senhaEncoded);
+		
+		System.out.println("Senha Válida: " + SenhaUtils.senhaValida("123456", senhaEncoded));
+	}
 }
--- src/test/java/br/com/luke/api/ApplicationApiApplicationTests.java
+++ src/test/java/br/com/luke/api/ApplicationApiApplicationTests.java
@@ -1,16 +1,28 @@
 package br.com.luke.api;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("develop")
 public class ApplicationApiApplicationTests {
+	
+	@Value("${paginacao.qtd_por_pagina}")
+	private int qtdPorPagina;
 
 	@Test
 	public void contextLoads() {
 	}
-
+	
+	public void testCarregarContextoDeTeste() {
+		assertEquals(100, qtdPorPagina);
+	}
 }
